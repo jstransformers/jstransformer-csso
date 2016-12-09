880e98a1cb28f4707dbb420182b7a0692abd7824
Rob Loach <robloach@gmail.com>
2016-12-09 13:27:45 -0500

    Add xo as coding style (#37)

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-'use strict';
+'use strict'
 
-var csso = require('csso');
+var csso = require('csso')
 
-exports.name = 'csso';
-exports.inputFormats = ['csso', 'css'];
-exports.outputFormat = 'css';
+exports.name = 'csso'
+exports.inputFormats = ['csso', 'css']
+exports.outputFormat = 'css'
 
 exports.render = function (str, options) {
-  return csso.minify(str, options).css;
-};
+  return csso.minify(str, options).css
+}
